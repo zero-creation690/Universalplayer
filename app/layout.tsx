0446350fcabc962c50978be89968be10c48a35c7
zero-creation690 <zerocration2128@gmail.com>
2025-07-12 17:25:23 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+// app/layout.tsx
+
+import "./globals.css"
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "UniversalPlayer",
+  description: "Play any movie with multi-audio & subtitles",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
